e564bb6be2828dcc977b2eb3dc690de780cf4845
Philip Maass <pmaass@databay.de>
2019-07-18 14:54:01 +0200

    fixes server parameter

--- src/Driver/IMAPDriver.php
+++ src/Driver/IMAPDriver.php
@@ -35,7 +35,7 @@ class IMAPDriver implements MailDriverInterface
      */
     public function __construct(array $config)
     {
-        $this->server = new Server($config['domain'], $config['port'], $config['flags']);
+        $this->server = new Server($config['server'], $config['port'], $config['flags']);
         $this->connection = $this->connect($config);
     }
 

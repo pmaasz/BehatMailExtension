ab1660b3eec352a68e6d6ed89d4e26bfbc624a9e
Philip Maass <pmaass@databay.de>
2020-01-10 13:48:35 +0100

    implements Singleton

---
+++ src/Service/Singleton.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace BehatMailExtension\Service;
+
+/**
+ * Trait Singleton
+ *
+ * @package BehatMailExtension\Service
+ */
+trait Singleton
+{
+    protected static $instance;
+
+    /**
+     * Singleton constructor.
+     */
+    protected function __construct()
+    {
+    }
+
+    /**
+     * clones Object we want to use when calling the instance
+     */
+    protected function __clone()
+    {
+    }
+
+    /**
+     * @return mixed
+     */
+    public static function getInstance()
+    {
+        if( ! self::$instance )
+        {
+            self::$instance = new self();
+        }
+        
+        return self::$instance;
+    }
+}

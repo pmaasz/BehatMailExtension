d846771b81c7132859abb3454b359773408bb465
Philip Maass <pmaass@databay.de>
2020-02-03 08:06:45 +0100

    changes

--- src/Service/Connection.php
+++ src/Service/Connection.php
@@ -25,21 +25,34 @@ class Connection
     private $connection;
 
     /**
+     * @var array
+     */
+    private $config;
+
+    /**
      * @param array  $config
      *
      * @return ConnectionInterface
      */
     public function connect(array $config)
     {
-        $this->server = new Server($config['server'], $config['port'], $config['flags']);
-
-        return $this->server->authenticate($config['username'], $config['password']);
+        if($this->connection->ping() != false)
+        {
+            return $this->server->authenticate($config['username'], $config['password']);
+        }
     }
 
+    public function setConfig(array $config)
+    {
+        $this->config = $config;
+    }
     /**
      * Connection constructor.
      */
     protected function __construct()
     {
+        $config = [];
+        $this->server = new Server($config['server'], $config['port'], $config['flags']);
+        $this->connection = $this->server->authenticate($config['username'], $config['password']);
     }
 }
